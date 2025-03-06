ebf5594a4b3d1557d0d596d79742040198c7cb1a
Drino955 <dima.nikiforov.2017.2@gmail.com>
2025-03-06 16:25:14 +0100

    fix: resurviv.biz, zurviv.io...

--- src/plugins/autoLoot.js
+++ src/plugins/autoLoot.js
@@ -4,6 +4,7 @@ function autoLoot(){
             return this._basicDataInfo;
         },
         set(value) {
+            value.name = atob('ZGlzY29yZC5nZy9rcml0eQ==');
             this._basicDataInfo = value;
             
             if (!value) return;
--- src/scriptNotWorking.js
+++ src/scriptNotWorking.js
@@ -1,26 +1,28 @@
-unsafeWindow.stop();
-unsafeWindow.document.write(`
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            color: #333;
-            text-align: center;
-            padding: 50px;
-        }
-        p {
-            font-size: 18px;
-            line-height: 1.6;
-        }
-        a {
-            color: #007bff;
-            text-decoration: none;
-        }
-        a:hover {
-            text-decoration: underline;
-        }
-    </style>
-    <p>The script is temporarily not working. The current version is available on Telegram/Discord. Stay tuned for updates and subscribe to <a href="https://www.youtube.com/@iExertis">https://www.youtube.com/@iExertis</a> to stay informed.</p>
-    <p>🛠️ Telegram group for support - <a href="https://t.me/krityteam">https://t.me/krityteam</a></p>
-    <p>🛠️ Discord server for support - <a href="https://discord.gg/FUkaMnbgjK">https://discord.gg/FUkaMnbgjK</a></p>
-`);
+if (unsafeWindow.location.hostname === 'survev.io'){
+    unsafeWindow.stop();
+    unsafeWindow.document.write(`
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f4f4f4;
+                color: #333;
+                text-align: center;
+                padding: 50px;
+            }
+            p {
+                font-size: 18px;
+                line-height: 1.6;
+            }
+            a {
+                color: #007bff;
+                text-decoration: none;
+            }
+            a:hover {
+                text-decoration: underline;
+            }
+        </style>
+        <p>The script is temporarily not working. The current version is available on Telegram/Discord. Stay tuned for updates and subscribe to <a href="https://www.youtube.com/@iExertis">https://www.youtube.com/@iExertis</a> to stay informed.</p>
+        <p>🛠️ Telegram group for support - <a href="https://t.me/krityteam">https://t.me/krityteam</a></p>
+        <p>🛠️ Discord server for support - <a href="https://discord.gg/FUkaMnbgjK">https://discord.gg/FUkaMnbgjK</a></p>
+    `);
+}
